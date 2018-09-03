8b6f83cfbb148d6e06b1b7baf73fe0850467dc2a
edgeboy47 <dmills1994@gmail.com>
2018-09-03 14:35:36 -0400

    Shows list of currently available movies

--- index.js
+++ index.js
@@ -1,4 +1,51 @@
 const puppeteer = require('puppeteer');
 
+let browser;
+let page;
+
 const comingSoonURL = 'http://movietowne.com/cinemas/comingsoon/';
 const nowShowingURL = 'http://movietowne.com/cinemas/nowshowing/';
+
+const locationStrings = {
+    pos: 'port-of-spain',
+    vipPos: 'vip-platinum-pos',
+    chag: 'chaguanas',
+    sando: 'san-fernando',
+    vipSando: 'platinum-san-fernando',
+    bago: 'tobago'
+}
+
+let nowShowingMovies = [];
+let comingSoonMovies = [];
+
+async function populateNowShowing(nowShowingMoviesArray){
+    const thumbnailSelector = 'div.movie_thumbnail';
+    await page.goto(`${nowShowingURL}${locationStrings.pos}`);
+    await page.waitForSelector(thumbnailSelector);
+    nowShowingMovies = await page.evaluate(() => Array.from(document.querySelectorAll('div.movie_thumbnail')).map( el => el.children[1].textContent));
+}
+
+function populateComingSoon(){
+
+}
+
+async function init(){
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+}
+
+async function end(){
+    await browser.close();
+}
+
+async function run(){
+    await init();
+    await populateNowShowing();
+    await end();
+    sortList(nowShowingMovies).forEach(el => console.table(el));
+}
+
+// Removes duplicates and sorts list
+const sortList = (list) => [...new Set(list)].sort();
+
+run();
